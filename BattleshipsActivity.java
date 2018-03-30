9635f7320186bf468e4b9a1df7440328157e2cf3
Szymon Lach <szymom.sp1@gmail.com>
2018-03-30 13:13:32 +0200

    30.03.18 Pierwszy commit

---
+++ BattleshipsActivity.java
@@ -0,0 +1,13 @@
+package com.example.szymi44.bluetoothapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BattleshipsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battleships);
+    }
+}
